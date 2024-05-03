cd929257c7c8163ffa09790567b4cb40720efc0f
Pierre-Luc Gagné <152034892+DisciplinedSoftware@users.noreply.github.com>
2024-05-03 02:58:43 +0000

    Add test around compute_square_root_digit_by_digit_method with stream

--- src/test/square_root_test.cpp
+++ src/test/square_root_test.cpp
@@ -1,5 +1,8 @@
 #include "../square_root.hpp"
 
+#include <thread>
+#include <sstream>
+
 #include <catch2/catch_approx.hpp>
 #include <catch2/catch_test_macros.hpp>
 
@@ -59,6 +62,35 @@ TEST_CASE("Square root") {
         CHECK(compute_square_root_digit_by_digit_method(99, 1) == "9.9"s);
         CHECK(compute_square_root_digit_by_digit_method(9999, 0) == "100"s);
         CHECK(compute_square_root_digit_by_digit_method(999999, 1) == "1000"s);
+    }
+
+    SECTION("compute_square_root_digit_by_digit_method with stream") {
+        using namespace std::string_literals;
+        std::ostringstream stream;
+        std::stop_token stop;
+
+        compute_square_root_digit_by_digit_method(stream, -1, stop);
+        CHECK(stream.str() == "nan"s);
+        stream = std::ostringstream();
+        compute_square_root_digit_by_digit_method(stream, 0, stop);
+        CHECK(stream.str() == "0"s);
+        stream = std::ostringstream();
+        compute_square_root_digit_by_digit_method(stream, -0, stop);
+        CHECK(stream.str() == "0"s);
+        stream = std::ostringstream();
+        compute_square_root_digit_by_digit_method(stream, 1, stop);
+        CHECK(stream.str() == "1"s);
+        stream = std::ostringstream();
+        compute_square_root_digit_by_digit_method(stream, 4, stop);
+        CHECK(stream.str() == "2"s);
+        stream = std::ostringstream();
+
+        std::jthread thread([&stream](std::stop_token stop_) {compute_square_root_digit_by_digit_method(stream, 42, stop_);});
+        while (stream.str().length() < 102) { std::this_thread::yield(); }
+        thread.request_stop();
+        thread.join();
+        auto result = stream.str().substr(0, 102);
+        CHECK(result == "6.4807406984078602309659674360879966577052043070583465497113543978096173778440443714003609066056102356"s);
     }
 
 #ifdef __GNUC__
