831c4bb3d6febf0fd09f05e7218cd826d02a5272
Pierre-Luc Gagné <152034892+DisciplinedSoftware@users.noreply.github.com>
2024-05-03 02:00:48 +0000

    Add a wait before launching the infinite stream

--- src/main.cpp
+++ src/main.cpp
@@ -25,6 +25,13 @@ void print_using_std_printf(const char* format_, unsigned int counter_, T value_
 }
 
 // ----------------------------------------------------------------------------
+// Utility
+void wait_for_enter_to_be_pressed() {
+    char c;
+    std::cin.get(c);
+}
+
+// ----------------------------------------------------------------------------
 int main(int argc, const char* argv[]) {
     const int result = Catch::Session().run(argc, argv);
 
@@ -86,12 +93,12 @@ int main(int argc, const char* argv[]) {
 
     std::cout << ++counter << ". Using infinite digits streaming:\n";
 
-    std::cout << "Press enter to quit...\n";
+    std::cout << "Press Enter to start streaming and Enter quit...\n";
+    wait_for_enter_to_be_pressed();
+
     std::jthread worker([](std::stop_token stop_) { compute_square_root_digit_by_digit_method(std::cout, 42, stop_); });
 
-    char c;
-    std::cin.get(c);
-
+    wait_for_enter_to_be_pressed();
     worker.request_stop();
 
     return result;
