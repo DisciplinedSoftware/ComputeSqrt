703a6b9078593ad585f14160aa024f5058aadca2
Pierre-Luc Gagné <152034892+DisciplinedSoftware@users.noreply.github.com>
2024-05-06 02:14:53 +0000

    Improve message

--- src/main.cpp
+++ src/main.cpp
@@ -18,7 +18,7 @@ void wait_for_enter_to_be_pressed() {
 int main(int argc, const char* argv[]) {
     const int result = Catch::Session().run(argc, argv);
 
-    std::cout << "Press Enter to start streaming and Enter to quit...\n";
+    std::cout << "Press Enter to start streaming the square root of 42 and Enter to quit...\n";
     wait_for_enter_to_be_pressed();
 
     std::jthread worker([](std::stop_token stop_) { compute_square_root_digit_by_digit_method(std::cout, 42, stop_); });
